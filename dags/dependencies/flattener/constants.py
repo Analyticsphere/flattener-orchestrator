0c43abac2eda530f29ff392e9f3d0cd5f7992aaf
Eddy Frankenberger <edward.frankenberger@nih.gov>
2025-03-18 15:33:29 -0400

    all functionality

--- dags/dependencies/flattener/constants.py
+++ dags/dependencies/flattener/constants.py
@@ -1,4 +1,4 @@
-#PROCESSOR_ENDPOINT = "https://ccc-flattener-eaf-dev-1061430463455.us-central1.run.app"
+#ROCESSOR_ENDPOINT = "https://ccc-flattener-eaf-dev-1061430463455.us-central1.run.app"
 PROCESSOR_ENDPOINT= "https://ccc-flattener-155089172944.us-central1.run.app"
 
 #BQ_PROJECT_ID = "nih-nci-dceg-connect-dev"
@@ -7,6 +7,8 @@ BQ_RAW_DATASET = "Connect"
 BQ_FLATTENED_DATASET = "FlatConnect"
 #GCS_FLATTENED_BUCKET = "flattener_tmp_dev"
 GCS_FLATTENED_BUCKET = "flattener_tmp"
+
+FIRESTORE_REFRESH_TOPIC = "schedule-firestore-backup"
 
 RAW_TABLES = [
     "bioSurvey_v1",
--- dags/flattener.py
+++ dags/flattener.py
@@ -2,14 +2,16 @@ from datetime import timedelta
 
 import dependencies.flattener.utils as utils
 import dependencies.flattener.constants as constants
-import dependencies.flattener.bq as bq
+import dependencies.flattener.gcp as gcp
 import dependencies.flattener.flatten as flatten
 
-
+import time
 import airflow # type: ignore
 from airflow import DAG  # type: ignore
 from airflow.decorators import task  # type: ignore
 from airflow.utils.dates import days_ago  # type: ignore
+from airflow.models.param import Param  # type: ignore
+from airflow.exceptions import AirflowSkipException  # type: ignore
 
 default_args = {
     'start_date': airflow.utils.dates.days_ago(1),
@@ -22,7 +24,14 @@ dag = DAG(
     'flattener',
     default_args=default_args,
     description='Pipeline for flattening Connect study data',
-    start_date=days_ago(1)
+    start_date=days_ago(1),
+    params={
+        'trigger_firestore_backup': Param(
+            default=False,
+            type='boolean',
+            description='When set to True, triggers Firestore backup and data refresh'
+        )
+    }
 )
 
 @task()
@@ -43,30 +52,52 @@ def check_api_health() -> None:
         raise Exception(f"API health check failed: {str(e)}")
 
 @task()
+def firestore_backup(**context) -> None:
+    """
+    Task to backup Firestore data.
+    Only executes if trigger_firestore_backup parameter is True.
+    Otherwise, it raises an AirflowSkipException to mark the task as skipped.
+    """
+    if not context['params']['trigger_firestore_backup']:
+        utils.logger.info("Skipping Firestore backup as parameter is set to False")
+        raise AirflowSkipException
+    else:
+        try:
+            gcp.pubsub_firestore_backup(constants.BQ_PROJECT_ID, constants.FIRESTORE_REFRESH_TOPIC)
+
+            # Wait 60 seconds for Firestore backup to complete; usually takes <1 second
+            time.sleep(60)
+        except Exception as e:
+            raise Exception(f"Unable to backup/refresh Firestore data: {str(e)}")
+
+@task(trigger_rule="none_failed")
 def table_to_parquet(table_name: str) -> None:
     try:
-        bq.bq_to_parquet(constants.BQ_PROJECT_ID, constants.BQ_RAW_DATASET, table_name, constants.GCS_FLATTENED_BUCKET)
+        gcp.bq_to_parquet(constants.BQ_PROJECT_ID, constants.BQ_RAW_DATASET, table_name, constants.GCS_FLATTENED_BUCKET)
     except Exception as e:
-        raise Exception(f"Unable to save {table_name} as Parquet")
+        raise Exception(f"Unable to save {table_name} as Parquet: {str(e)}")
 
-@task()
+@task(trigger_rule="none_failed")
 def flatten_parquet(table_name: str) -> None:
     try:
         flatten.flatten_parquet(table_name, constants.GCS_FLATTENED_BUCKET)
     except Exception as e:
         raise Exception(f"Unable to flatten {table_name} Parquet files: {str(e)}")
 
-@task()
+@task(trigger_rule="none_failed")
 def parquet_to_table(table_name: str) -> None:
     try:
-        bq.parquet_to_bq(constants.BQ_PROJECT_ID, constants.BQ_FLATTENED_DATASET, table_name, constants.GCS_FLATTENED_BUCKET)
+        gcp.parquet_to_bq(constants.BQ_PROJECT_ID, constants.BQ_FLATTENED_DATASET, table_name, constants.GCS_FLATTENED_BUCKET)
     except Exception as e:
         raise Exception(f"Error moving {table_name} Parquet file into BigQuery: {str(e)}")
 
 with dag:
     api_health_check = check_api_health()
+    firestore_refresh = firestore_backup()
+    
     bq_to_parquet = table_to_parquet.expand(table_name=constants.RAW_TABLES)
     flatten_files = flatten_parquet.expand(table_name=constants.RAW_TABLES)
     parquet_to_bq = parquet_to_table.expand(table_name=constants.RAW_TABLES)
 
-    api_health_check >> bq_to_parquet >> flatten_files >> parquet_to_bq
+    # Simple linear dependency chain
+    api_health_check >> firestore_refresh >> bq_to_parquet >> flatten_files >> parquet_to_bq
